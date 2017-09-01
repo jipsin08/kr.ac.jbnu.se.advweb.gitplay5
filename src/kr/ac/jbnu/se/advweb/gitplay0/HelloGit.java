75439d298f7c18229cf3ad7e4bffc6d31ad6dd91
SuntaeKim <stkim@jbnu.ac.kr>
2017-09-01 11:12:32 +0900

    initial commit

---
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
@@ -0,0 +1,20 @@
+package kr.ac.jbnu.se.advweb.gitplay0;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+		// how are you doing??
+		System.out.println("Hello Git!!???????????");
+		
+		// Okay I'll change this part as below:
+		// I've just added the following part in Eclipse
+		for(int i = 0;i < 100; i++)
+		{
+			System.out.println("i:" + i);
+		}
+		
+		// I added the following part.
+		System.out.println("This is a confliction test.");
+	}
+}
---
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
@@ -0,0 +1,10 @@
+package kr.ac.jbnu.se.advweb.gitplay0;
+
+public class HelloGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+		
+		// how are you doing??
+	}
+}
