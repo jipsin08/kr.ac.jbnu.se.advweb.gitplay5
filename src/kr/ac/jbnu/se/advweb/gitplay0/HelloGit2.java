a449d4e077423f83288a53e108944e5c83fcf86a
LG <LG@DESKTOP-0V89BIB>
2017-09-25 16:40:07 +0900

    Git 테스트용

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
@@ -5,6 +5,8 @@ public class HelloGit2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("아아아아아악");
+		
+		System.out.println("GIt!!!!!!");
 		// how are you doing??
 	}
 }
